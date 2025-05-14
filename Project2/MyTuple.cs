9f170a00698c8acbb4f89fcb586fd25613da7057
beatriskr <beti.todorova123@gmail.com>
2025-05-14 12:35:36 +0300

    Add files via upload

---
+++ Project2/MyTuple.cs
@@ -0,0 +1,19 @@
+﻿namespace Project2
+{
+    public class MyTuple
+    {
+        public int Value1 { get; set; }
+        public int Value2 { get; set; }
+
+        public MyTuple(int v1, int v2)
+        {
+            Value1 = v1;
+            Value2 = v2;
+        }
+
+        public override string ToString()
+        {
+            return $"{Value1}, {Value2}";
+        }
+    }
+}
---
+++ Project2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Project2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string filePath = "data.txt";
+            long fn = 22251421008;
+            int n = (int)(fn % 4 + 2);
+            Console.WriteLine($"n = {n}");
+
+            int targetLastDigit = int.Parse(fn.ToString()[fn.ToString().Length - 2].ToString());
+
+            List<MyTuple> allTuples = new List<MyTuple>();
+
+            foreach (string line in File.ReadAllLines(filePath))
+            {
+                string[] parts = line.Split(',');
+
+                if (parts.Length == n)
+                {
+                    int val1 = int.Parse(parts[0].Trim());
+                    int val2 = int.Parse(parts[1].Trim());
+                    allTuples.Add(new MyTuple(val1, val2));
+                }
+            }
+
+            var matching = allTuples.Where(t => t.Value2 % 10 == targetLastDigit);
+
+            Console.WriteLine("Matching tuples:");
+            foreach (var t in matching)
+            {
+                Console.WriteLine(t);
+            }
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
